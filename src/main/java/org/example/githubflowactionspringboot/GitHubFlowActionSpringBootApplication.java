ad6854d5c48b4b5991faafbe2cfbaf2344bb915b
maztas-tech <mata0001@stud.kea.dk>
2024-02-27 09:56:45 +0100

    Initial commit

---
+++ src/main/java/org/example/githubflowactionspringboot/GitHubFlowActionSpringBootApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubflowactionspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubFlowActionSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubFlowActionSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/githubflowactionspringboot/GitHubFlowActionSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.githubflowactionspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubFlowActionSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
